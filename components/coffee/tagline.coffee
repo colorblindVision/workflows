a24030111633361779991c5d2856d1e26361b1ec
dan.freeman <dan.freeman@wolterskluwer.com>
2015-10-01 16:11:35 -0400

    adding jquery and modules and browserify

--- components/coffee/tagline.coffee
+++ components/coffee/tagline.coffee
@@ -1,3 +1,5 @@
+$ = require 'jquery'
+
 do fill = (item = 'This is dynamic content from coffee script') ->
 	$('.tagline').append "#{item}"
 fill
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
 gulp.task('log', function(){
 	gutil.log('workflows are awesome!');
@@ -20,5 +21,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('js.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
