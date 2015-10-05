c4237352808650cf395a9d174ea5625519294008
dan.freeman <dan.freeman@wolterskluwer.com>
2015-10-05 17:34:40 -0400

    adding image compression

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	minifyHTML = require('gulp-minify-html'),
 	jsonminify = require('gulp-jsonminify'),
+	imagemin = require('gulp-imagemin'),
+	pngcrush = require('imagemin-pngcrush'),
 	concat = require('gulp-concat');
 
 var env, 
@@ -19,8 +21,8 @@ var env,
 	outputDir,
 	sassStyle;
 	
-//env = process.env.NODE_ENV || 'development';
-env = process.env.NODE_ENV || 'production';
+env = process.env.NODE_ENV || 'development';
+//env = process.env.NODE_ENV || 'production';
 	
 if (env === 'development') {
 	outputDir = 'builds/development/';
@@ -68,7 +70,7 @@ gulp.task('compass', function(){
 		.pipe(connect.reload())
 });
 
-gulp.task('default', ['html', 'coffee', 'js', 'json', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'coffee', 'js', 'json', 'compass', 'images', 'connect', 'watch']);
 
 gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
@@ -76,6 +78,7 @@ gulp.task('watch', function(){
 	gulp.watch('builds/development/*html', ['html']);
 	gulp.watch('builds/development/js/*.json', ['json']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch('builds/development/images/**/*.*', ['images']);
 });
 
 gulp.task('connect', function(){
@@ -89,6 +92,17 @@ gulp.task('html', function(){
 	gulp.src('builds/development/*html')
 		.pipe(gulpif(env === 'production', minifyHTML()))
 		.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+		.pipe(connect.reload())
+});
+
+gulp.task('images', function(){
+	gulp.src('builds/development/images/**/*.*')
+		.pipe(gulpif(env === 'production', imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngcrush()]
+		})))
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
 		.pipe(connect.reload())
 });
 
